b1549f87eb7e71b5543bfa2f9071699f2a0deb60
VideoCheese3773 <eedduxx@gmail.com>
2023-02-14 08:26:34 -0500

    copied and pasted, it worked

--- characters.js
+++ characters.js
@@ -1,19 +1,52 @@
-export const renderCharacter({name, thumbnail, description, comics, series}) => {
-    const figure = document.createElement("figure");
-    const img = document.createElement("img");
-    const p = document.createElement("p");
-    const ulComics = document.createElement("ul");
-    const ulSeries = document.createElement("ul");
-    const title = document.createElement("h3");
-
-    img.src = thumbnail;
-    p.textContent = description;
-    title.textContent = name;
-
-    figure.appendChild(img);
-    figure.appendChild(title);
-    figure.appendChild(p);
-
-}
-
-//fuck this bullshit bro
+export default [
+{
+    name: "Spider-Man",
+    thumbnail:
+        "https://as01.epimg.net/meristation/imagenes/2021/10/25/noticias/1635151110_716951_1635151285_noticia_normal.jpg",
+    description:
+        "Peter Parker, mejor conocido como Spider-Man, es un superhéroe que posee la capacidad de tejer telarañas y trepar paredes. Después de ser mordido por una araña radioactiva, Peter adquirió estas habilidades sobrehumanas, que utiliza para combatir el crimen en la ciudad de Nueva York.",
+    comics: [
+        "Amazing Fantasy #15",
+        "The Amazing Spider-Man #1",
+        "Spider-Man: Blue",
+    ],
+    series: [
+        "The Amazing Spider-Man",
+        "Ultimate Spider-Man",
+        "Spider-Man and His Amazing Friends",
+    ],
+},
+{
+    name: "Iron Man",
+    thumbnail: "https://images.news18.com/ibnlive/uploads/2016/04/ironman.jpg",
+    description:
+        "Tony Stark, mejor conocido como Iron Man, es un multimillonario empresario y genio de la tecnología que desarrolló una armadura que le otorga increíbles habilidades y lo convierte en un superhéroe. Como Iron Man, Tony lucha contra amenazas como el Mandarín, el Doctor Doom y el villano Obadiah Stane.",
+    comics: [
+        "Tales of Suspense #39",
+        "The Invincible Iron Man #1",
+        "Iron Man: Extremis",
+    ],
+    series: [
+        "Iron Man",
+        "The Invincible Iron Man",
+        "Iron Man and the Armor Wars",
+    ],
+},
+{
+    name: "Captain America",
+    thumbnail:
+        "https://terrigen-cdn-dev.marvel.com/content/prod/1x/003cap_ons_crd_03.jpg",
+    description:
+        "Steve Rogers, mejor conocido como Captain America, es un superhéroe y el líder de los Vengadores. Después de ser sometido a un experimento secreto durante la Segunda Guerra Mundial, Steve adquirió una fuerza y resistencia sobrehumanas, lo que le permite luchar contra el mal en defensa de su país y del mundo.",
+    comics: [
+        "Captain America Comics #1",
+        "Captain America: Winter Soldier",
+        "Captain America: The Death of Captain America",
+    ],
+    series: [
+        "Captain America",
+        "Captain America and the Falcon",
+        "The Ultimates",
+    ],
+},
+];
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //https://github.com/LeiderCalvo/DCA/tree/05_modules
-
-import {data} from "./module.js";
-console.log(data);
+import characters from "./characters.js";
+import { renderCharacter } from "./Character.js";
 
 const container = document.getElementById("app");
 
+characters.forEach((character) =>
+    container.appendChild(renderCharacter(character))
+);
